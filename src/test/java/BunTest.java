c9de1bf2a8ae0b7319f89e45bba45b4d1c945130
Kseniia Seriapina <mimi.kseni@gmail.com>
2024-02-28 21:44:20 +0000

    Добавлены тесты, обновлен файл pom.xml и добавлен отчёт Jacoco

--- src/test/java/BunTest.java
+++ src/test/java/BunTest.java
@@ -1,36 +1,26 @@
 import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.junit.runners.Parameterized;
 import praktikum.Bun;
-
-import java.util.Arrays;
-import java.util.Collection;
 
 import static org.junit.Assert.assertEquals;
 
-@RunWith(Parameterized.class)
 public class BunTest {
-
-    private final String name;
-    private final float price;
-
-    public BunTest(String name, float price) {
-        this.name = name;
-        this.price = price;
-    }
-
-    @Parameterized.Parameters
-    public static Collection<Object[]> data() {
-        return Arrays.asList(new Object[][] {
-                {"black bun", 100},
-                {"white bun", 200}
-        });
-    }
 
     @Test
     public void testBunNameAndPrice() {
-        Bun bun = new Bun(name, price);
-        assertEquals("Название булочки должно соответствовать", name, bun.getName());
-        assertEquals("Цена булочки должна соответствовать", price, bun.getPrice(), 0.01);
+        // Создаю объект Bun с названием "black bun" и ценой 100
+        Bun blackBun = new Bun("black bun", 100);
+
+        // Проверяю, что название и цена соответствует созданному
+        assertEquals("Название булочки должно быть 'black bun'", "black bun", blackBun.getName());
+        assertEquals("Цена булочки должна быть 100", 100, blackBun.getPrice(),  0.01);
+
+        // Создаю объект Bun с названием "white bun" и ценой 200
+        Bun whiteBun = new Bun("white bun", 200);
+
+        // Проверяю, что название и цена соответствует созданному
+        assertEquals("Название булочки должно быть 'white bun'", "white bun", whiteBun.getName());
+        assertEquals("Цена булочки должна быть 200", 200, whiteBun.getPrice(),0.01);
+
+
     }
 }
